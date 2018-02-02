ee200b513f2f0be55a819e4334ef5be0799203ba
SusanaPineda <susana.pineda.de.luelmo@gmail.com>
2018-02-02 18:38:30 +0100

    prueba2

--- guardar_punto/src/main/java/es/sidelab/guardar_punto/App.java
+++ guardar_punto/src/main/java/es/sidelab/guardar_punto/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "a ver si somos capaces!" );
+        System.out.println( "ver si somos capaces!" );
     }
 }
