a0ff711fcd4920184cdde39e467a55a49c647422
AdrianMorillas2 <32101147+AdrianMorillas2@users.noreply.github.com>
2018-02-02 17:41:22 +0100

    Actualizacion

---
+++ guardar_punto/src/main/java/es/sidelab/guardar_punto/App.java
@@ -0,0 +1,13 @@
+package es.sidelab.guardar_punto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
