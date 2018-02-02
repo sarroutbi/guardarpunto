ff5868a51b2f79d281f25804442c60399dac698d
Marta FMS <32145806+mfms5@users.noreply.github.com>
2018-02-02 19:32:08 +0100

    Prueba push

--- guardar_punto/src/main/java/es/sidelab/guardar_punto/App.java
+++ guardar_punto/src/main/java/es/sidelab/guardar_punto/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "ver si somos capaces!" );
-        //comentario de marta
+        //comentario de marta editado desde git
     }
 }
