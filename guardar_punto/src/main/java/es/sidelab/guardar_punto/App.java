afca261cb21dea6220c775f6c069bb531da67e08
Marta FMS <32145806+mfms5@users.noreply.github.com>
2018-02-02 19:37:46 +0100

    Comentario eliminado

--- guardar_punto/src/main/java/es/sidelab/guardar_punto/App.java
+++ guardar_punto/src/main/java/es/sidelab/guardar_punto/App.java
@@ -9,6 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "ver si somos capaces!" );
-        //comentario de marta editado desde git
     }
 }
