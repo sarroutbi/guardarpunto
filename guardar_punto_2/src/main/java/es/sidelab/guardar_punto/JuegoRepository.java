d1747465fda26a4d25e64cb602eb3f85f4a0f0a8
AdrianMorillas2 <32101147+AdrianMorillas2@users.noreply.github.com>
2018-02-10 12:55:48 +0100

    Add files via upload
    Añadida consulta para conseguir juegos por estado y usuario

--- guardar_punto_2/src/main/java/es/sidelab/guardar_punto/JuegoRepository.java
+++ guardar_punto_2/src/main/java/es/sidelab/guardar_punto/JuegoRepository.java
@@ -2,6 +2,8 @@ package es.sidelab.guardar_punto;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface JuegoRepository extends JpaRepository<Juego,Integer> {
 	List<Juego> findByTitle(String Titulo);
@@ -9,4 +11,8 @@ public interface JuegoRepository extends JpaRepository<Juego,Integer> {
 	List<Juego> findByAnyo(String anyo);
 	List<Juego> findByPlataforma(String Plataforma);
 	List<Juego> findByGenero(String Genero);
+	
+	/*Busca el juego en funcion del estado del juego y el id del usuario*/
+	@Query(value="SELECT j FROM Usuarios u,Estado e,Juego j WHERE e.estado=:estat AND u.id=:u_id")
+	List<Juego> findByEstadoYUser(@Param("estat") String es,@Param("u_id") Integer us);
  }
