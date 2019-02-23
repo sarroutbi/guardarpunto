67471dce18ed6d0b16eb7cc249e797106e8f8c84
Sergio Arroutbi Braojos <sergio.arroutbi.braojos@ericsson.com>
2019-02-23 11:59:08 +0100

    Remove targets, that must never be in repository

--- guardar_punto_2/target/classes/static/js/InicioJS.js
+++
@@ -1,9 +0,0 @@
-//Ocultar el panel de login para mostrar el de mis juegos
-function ocultarLogin() {
-    var login = document.getElementById("login");
-    var tusJuegos = document.getElementById("tusJuegos");
-
-    login.style.display = "none";
-    tusJuegos.style.display = "block";
-}
-
