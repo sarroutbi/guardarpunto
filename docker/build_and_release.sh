f81f8730cb3a1b96bdf76cbf0336342494b3ce10
Sergio Arroutbi Braojos <sergio.arroutbi.braojos@ericsson.com>
2019-03-02 21:57:37 +0100

    Set container labels to statement specification

--- docker/build_and_release.sh
+++ docker/build_and_release.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+IMAGE_LABEL=sergioarroutbibraojos
 for dir in $(find . -type d); 
 do
   pushd $dir
   image=$(basename $(pwd))
   docker build -t=${image} .
-  docker tag ${image}:latest sarroutbi/${image}:latest
-  docker push sarroutbi/${image}:latest
+  docker tag ${image}:latest sarroutbi/${image}:${IMAGE_LABEL}
+  docker push sarroutbi/${image}:${IMAGE_LABEL}
   popd
 done 
